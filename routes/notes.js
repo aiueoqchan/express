10d9c522a7a143a5f3f791ac01216844722175da
aiueoqchan <aiueo_qchan@yahoo.co.jp>
2024-12-03 21:53:44 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-const responseObjectDataAll = {
-textObject1 : {
-id: 1,
-title: 'ノート1のタイトルです',
-subTitle: 'ノート1のサブタイトルです',
-bodyText: 'ノート1の本文です',
-},
-textObject2 : {
-id: 2,
-title:'ノート2のタイトルです',
-subTitle:'ノート2のサブタイトルです',
-bodyText:'ノート2の本文です'
-},
-};
 
-router.get('/', function (req, res, next){
+
+//setuzoku jyouhou setting
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://2201110058zu:01252002@cluster0.9e0tp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req,  res) => {
+  //database collection setting
+  const database = client.db('notes');
+  const notes = database.collection('notes');
+
+//id ga 1 no document shutoku
+  const query = {id: 2 };
+  const note = await notes.findOne(query);
 
 res.json(responseObjectDataAll);
 })
